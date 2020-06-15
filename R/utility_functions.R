bde3d42e25baa71d2d394acdc4b050ea3b7b89cf
livio finos <livio@stat.unipd.it>
2020-06-15 02:13:19 +0200

    check for submission

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -1,22 +1,24 @@
-get_head_flip_out <- function(x){
-  if(length(grep("Negative Binomial",x$family$family))==0)
-  {paste("Flip Score Test: 
-         score_type =",x$score_type,
-         ", n_flips =",x$n_flips,"\n")}
-  else 
-    paste("Flip Score Test: 
-          score_type =",x$score_type,
-          ", n_flips =",x$n_flips,
-          ", theta =",round(x$theta,digits=5),"\n")
-}
+#' @title Methods for flipscores objects
+#'
+#' @description Methods for \code{flipscores} objects. 
+#' The following are methods to extract and manipulate relevant information from
+#' a \code{flipscores-object}.
+#' 
+#' @name flipscores-method
+#' @rdname flipscores-method
+#' @docType methods
+
+NULL
+
 
 
 #' print.flipscores print method for a flipscores-object.
-#' @rdname print.flipscores
 #' @param x a flipscores-object
 #' @param ... additional arguments to be passed
 #' @exportMethod print
-# @docType methods
+#' @docType methods
+#' @rdname flipscores-method
+
 
 print.flipscores <- function(x, ...) {
   cat(get_head_flip_out(x))
@@ -28,10 +30,11 @@ print.flipscores <- function(x, ...) {
 }
 
 #' summary.flipscores summary method for a flipscores-object.
-#' @rdname summary.flipscores
-#' @param object a flipscores-object
+#' @rdname flipscores-method
+#' @param object a flipscores object
 #' @param ... additional arguments to be passed
 #' @exportMethod  summary
+#' @docType methods
 
 summary.flipscores <- function (object, ...) {
   sum_model=summary.glm(object = object)
@@ -45,4 +48,18 @@ summary.flipscores <- function (object, ...) {
   
   structure(sum_model, heading = get_head_flip_out(object), class = c("data.frame"))
   sum_model
+}
+
+
+###########
+get_head_flip_out <- function(x){
+  if(length(grep("Negative Binomial",x$family$family))==0)
+  {paste("Flip Score Test: 
+         score_type =",x$score_type,
+         ", n_flips =",x$n_flips,"\n")}
+  else 
+    paste("Flip Score Test: 
+          score_type =",x$score_type,
+          ", n_flips =",x$n_flips,
+          ", theta =",round(x$theta,digits=5),"\n")
 }
