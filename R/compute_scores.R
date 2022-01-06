0cc9c1027da9b379cb4f906f7818543a4c7047c3
Rickdesa <73401704+Rickdesa@users.noreply.github.com>
2022-01-06 19:53:04 +0100

    Update compute_scores.R

--- R/compute_scores.R
+++ R/compute_scores.R
@@ -3,13 +3,13 @@
 #' @param model1 a \code{glm} or a \code{matrix} (or \code{vector}). If it is a \code{glm} object, it has the model under the alternative hypothesis. The variables in \code{model1} are the same variables in \code{model0} plus one or more variables to be tested.  Alternatively, if
 #' \code{model1} is a \code{matrix}, it contains the tested variables column-wise.
 #' @param score_type The type of score that is computed. It is "orthogonalized", "effective" or "basic".
-#' Default is "orthogonalized". "effective" and "orthogonalized" take into account the nuisance estimation.
+#' "effective" and "orthogonalized" take into account the nuisance estimation.
 #' @description 
 #' Same usage as \code{anova.glm}. 
 #' The parameter \code{id}  is used too, 
 #' if present in \code{model0} (with priority) or in \code{model1}.
 #'
-#' @author Jesse Hemerik, Vittorio Giatti, Jelle Goeman and Livio Finos
+#' @author Jesse Hemerik, Riccardo De Santis, Vittorio Giatti, Jelle Goeman and Livio Finos
 #' @examples
 #' Z=rnorm(20)
 #' X=Z+rnorm(20)
@@ -18,42 +18,46 @@
 #' (scr0=compute_scores(model0 = mod0, model1 = X, score_type = "effective"))
 #' @export
 
-compute_scores <- function(model0, model1,score_type="effective"){
+compute_scores <- function(model0, model1,score_type){
   score_type=match.arg(score_type,c("orthogonalized","standardized","effective","basic"))
+  if(missing(score_type))
+    stop("test type is not specified or recognized")
   X=get_X(model0,model1)
-  if(!is.null(model0))
+  if(!is.null(model0))  
   {id_model1=if(is.list(model1)) eval(model1$id) 
-    else 
-      NULL
+  else 
+    NULL
   } else id_model1=NULL
-
+  
   rm(model1)
-  a <- get_a_expo_fam(model0)
+  
   ###############
   if(is.null(model0$x)||is.null(ncol(model0$x))) {
-    # model0=update(model0,x=TRUE)
     call <- getCall(model0)
     call$x=TRUE
     term <- terms(model0)
     env <- attr(term, ".Environment")
     model0=eval(call, env, parent.frame())
   }
-
+  
   
   if(ncol(model0$x)==0) return(model0$y)
   Z=model0$x
-  residuals=(model0$y-model0$fitted.values)/a
+  residuals=(model0$y-model0$fitted.values)
   W=diag(as.numeric(model0$weights))
-
-
+  pars_score<-get_par_expo_fam(model0)
+  D<-pars_score$D
+  V<-pars_score$V
+  invV<-diag(diag(V**(-1)))
+  
   #BASIC SCORE
   if(score_type=="basic"){
-    scores=(X*(residuals))
+    scores=t(t(X)%*%D%*%(invV))*residuals*(1/length(model0$y))
   } else
     ##  EFFECTIVE SCORE
     if(score_type=="effective"){
-      OneMinusH=(diag(nrow(Z))-W%*%Z%*%solve(t(Z*diag(W))%*%Z)%*%t(Z))
-      scores=(OneMinusH%*%X)*residuals
+      OneMinusH=(diag(nrow(Z))-(W**0.5)%*%Z%*%solve(t(Z)%*%W%*%Z)%*%t(Z)%*%(W**0.5))
+      scores=t(t(X)%*%(W**0.5)%*%(OneMinusH))*((invV**0.5)%*%residuals)*(1/length(model0$y))
       
     } else
       ##   SCORE standardized
@@ -72,26 +76,10 @@ compute_scores <- function(model0, model1,score_type="effective"){
         #ORTHO EFFECTIVE SCORE
       if(score_type=="orthogonalized"){
         sqrtW=diag(sqrt(diag(W)))
-        # OneMinusHtilde_0=(diag(n)-sqrtW%*%Z%*%solve(t(Z*diag(W))%*%Z)%*%t(Z)%*%sqrtW)
-        OneMinusH=(diag(nrow(Z))-W%*%Z%*%solve(t(Z*diag(W))%*%Z)%*%t(Z))
-        deco=svd(OneMinusH* matrix(diag(sqrtW),dim(OneMinusH)[1],dim(OneMinusH)[1],byrow = TRUE),
-                 nv = 0)
-        
+        OneMinusH=(diag(nrow(Z))-(W**0.5)%*%Z%*%solve(t(Z)%*%W%*%Z)%*%t(Z)%*%(W**0.5))
+        deco=svd((V^0.5)%*%OneMinusH,nv = 0)
         deco$d[deco$d<1E-12]=0
-        scores=
-          (t(t(X)%*%OneMinusH%*%deco$u)*
-          (t(deco$u)%*%(residuals)))
-        
-      }
-  
-  # if(!is.null(model0$id)){
-  #   scores=rowsum(scores,eval(model0$id)) 
-  #   } else if(!is.null(id_model1)){
-  #     scores=rowsum(scores,id_model1)      
-  #   }
-  
-  # mean_adjustment
-  # median_adjustment
-  # 
+        scores=t(t(X)%*%sqrtW%*%OneMinusH%*%(invV**0.5)%*%deco$u)*(t(deco$u)%*%residuals)*(1/length(model0$y))
+  }
   return(scores)
 }
