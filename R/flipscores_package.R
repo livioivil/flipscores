c6795e56282d8a32887061ec18e5cd8c8f258b51
livio <livio.finos@unipd.it>
2022-06-29 12:59:24 +0200

    ...

--- R/flipscores_package.R
+++ R/flipscores_package.R
@@ -3,7 +3,6 @@
 #' @description It provides robust tests for testing in GLMs, by sign-flipping score contributions. The tests are often robust against overdispersion, heteroscedasticity and, in some cases, ignored nuisance variables.
 #' @importFrom car Anova
 #' @importFrom MASS glm.nb
-#' @importFrom plyr laply
 #' @importFrom methods is
 #' @importFrom stats D as.formula model.matrix sd summary.glm update
 #' @examples
